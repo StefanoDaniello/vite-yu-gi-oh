16dab22089af976cd5b487ed5a19d037d39eb466
StefanoDaniello <157887560+StefanoDaniello@users.noreply.github.com>
2024-04-15 15:30:02 +0200

    start

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-light">
+        <h1>Yu-Gi-Oh Api</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'HeaderComponent',
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { reactive } from "vue";
+
+export const store = reactive({
+    apiUrl: 'https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0',
+    endPoint:{
+        "name": "name",
+        "type": "type",
+        "race": "race",
+        // ci dice se una spell card un mostro ecc
+    }
+});
